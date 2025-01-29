3b1b97233df61437e5f37fce33e16a6c9a62ca8a
Aius3 <kajus.mocevicius@gmail.com>
2025-01-29 09:26:37 +0200

    paimtas pagrindas

---
+++ P47_Flask_webAPI/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+
+class Automobilis(db.Model):
+    __tablename__ = "automobiliai"
+    id = db.Column(db.Integer, primary_key=True)
+    gamintojas = db.Column(db.String)
+    modelis = db.Column(db.String)
+    spalva = db.Column(db.String)
+    metai = db.Column(db.Integer)
+    variklis = db.Column(db.String)
+    kaina = db.Column(db.Float)
+    sukurimo_data = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f"id: {self.id} {self.gamintojas} {self.modelis} {self.spalva} {self.metai} {self.variklis} {self.kaina} {self.sukurimo_data}"
