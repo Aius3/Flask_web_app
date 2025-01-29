d0d6395fa261d2de19996798e437521359ceab1c
Aius3 <kajus.mocevicius@gmail.com>
2025-01-29 10:04:43 +0200

    pataisymai

--- P47_Flask_webAPI/models.py
+++ P47_Flask_webAPI/models.py
@@ -3,8 +3,6 @@ import datetime
 
 db = SQLAlchemy()
 
-db.init_app(app)
-
 class Automobilis(db.Model):
     __tablename__ = "automobiliai"
     id = db.Column(db.Integer, primary_key=True)
@@ -18,11 +16,3 @@ class Automobilis(db.Model):
 
     def __repr__(self):
         return f"id: {self.id} {self.gamintojas} {self.modelis} {self.spalva} {self.metai} {self.variklis} {self.kaina} {self.sukurimo_data}"
-
-if __name__ == "__main__":
-    from app import app
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    all_cars = Automobilis.query.all()
-    print(all_cars)
